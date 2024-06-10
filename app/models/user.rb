ca5dd1cb4596a02a6d4a68ab2bdc4bc8a69734ef
parmi89 <edgarparmi@gmail.com>
2024-06-10 19:36:41 -0300

    Se refactoriza model user y admn con un concerns -> login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include Login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,23 +7,8 @@ class User < ApplicationRecord
          :validatable, authentication_keys: [:login]
 
   
-attr_writer :login
 
-def login
-  @login || self.username || self.email
-end
-
-def self.find_for_database_authentication(warden_conditions)
-  conditions = warden_conditions.dup
-  if (login = conditions.delete(:login))
-    where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-    where(conditions.to_h).first
-  end
-end
-
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
-  has_many :products, dependent: :destroy
+  
 end
 
 
