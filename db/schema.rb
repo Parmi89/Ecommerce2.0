e39ab99ff799881119392af21e78077f521e278d
parmi89 <edgarparmi@gmail.com>
2024-06-18 18:01:56 -0300

    Se agrega estilo a los formularios de devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_13_074723) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_184827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_074723) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_photos_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -137,6 +144,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_074723) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "info_users", "users"
+  add_foreign_key "photos", "products"
   add_foreign_key "products", "admins"
   add_foreign_key "products", "categories"
 end
