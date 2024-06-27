4c92f71eb5f622f7c50e7b28f86fed99f257469c
parmi89 <edgarparmi@gmail.com>
2024-06-27 16:34:11 -0300

    Se agrega show de usuarios especificos se completa traducciones

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,10 @@ class Admin::UsersController < ApplicationController
     @info_users = InfoUser.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
   end
